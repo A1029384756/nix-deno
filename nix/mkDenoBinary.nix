aa7d79d5ea01e5b2f2782c307164b4df6902ac2d
A1029384756 <hayden.gray104@gmail.com>
2024-12-18 16:18:29 -0500

    formatting

--- nix/mkDenoBinary.nix
+++ nix/mkDenoBinary.nix
@@ -1,31 +1,31 @@
-{
-  lib,
-  deno,
-  denoPlatform,
-}: {
-  name,
-  permissions ? {},
-  include ? [],
-  unstable ? false,
-  entryPoint ? "main.ts",
-  binaryName ? name,
-  additionalDenoArgs ? [],
-  scriptArgs ? [],
-  ...
-} @ args: let
+{ lib
+, deno
+, denoPlatform
+,
+}: { name
+   , permissions ? { }
+   , include ? [ ]
+   , unstable ? false
+   , entryPoint ? "main.ts"
+   , binaryName ? name
+   , additionalDenoArgs ? [ ]
+   , scriptArgs ? [ ]
+   , ...
+   } @ args:
+let
   compileArgs = denoPlatform.lib.generateFlags {
     inherit permissions unstable include entryPoint scriptArgs;
-    additionalDenoArgs = ["--output" binaryName] ++ additionalDenoArgs;
+    additionalDenoArgs = [ "--output" binaryName ] ++ additionalDenoArgs;
   };
 in
-  denoPlatform.mkDenoDerivation ({
-      # fixup corrupts the binary, leaving it in a Deno REPL-only state
-      dontFixup = true;
+denoPlatform.mkDenoDerivation ({
+  # fixup corrupts the binary, leaving it in a Deno REPL-only state
+  dontFixup = true;
 
-      buildPhase = "DENORT_BIN=${deno}/bin/denort deno compile ${compileArgs}";
-      installPhase = "install -Dm755 ${binaryName} $out/bin/${binaryName}";
+  buildPhase = "DENORT_BIN=${deno}/bin/denort deno compile ${compileArgs}";
+  installPhase = "install -Dm755 ${binaryName} $out/bin/${binaryName}";
 
-      # default to Deno's platforms
-      meta.platforms = deno.meta.platforms;
-    }
-    // (builtins.removeAttrs args ["permissions"]))
+  # default to Deno's platforms
+  meta.platforms = deno.meta.platforms;
+}
+// (builtins.removeAttrs args [ "permissions" ]))
